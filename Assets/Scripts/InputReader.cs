38e56fe3f21bb802389b543202a8a1c16a6fc6bb
double-hope <nadia.prohorchuk@gmail.com>
2023-03-23 15:11:20 +0200

    add assets

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -6,6 +6,7 @@ public class InputReader : MonoBehaviour
     [SerializeField] private PlayerController _playerController;
     [SerializeField] private PlayerAnimations _playerAnimations;
     private Vector2 _direction;
+    
     private void Update()
     {
         _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
