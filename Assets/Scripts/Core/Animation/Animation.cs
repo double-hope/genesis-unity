e6d1c0523402deffc4b695ed2e73d6f7d1fc9d71
double-hope <nadia.prohorchuk@gmail.com>
2023-03-27 00:07:13 +0300

    change PlayerAnimations to Animation

--- Assets/Scripts/Core/Animation/Animation.cs
+++ Assets/Scripts/Core/Animation/Animation.cs
@@ -1,32 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-public class Animations : MonoBehaviour
+namespace Core.Animation
 {
-    private Animator _animator;
-    private static readonly int MovementX = Animator.StringToHash("MovementX");
-    private static readonly int MovementY = Animator.StringToHash("MovementY");
-    private static readonly int isWalking = Animator.StringToHash("isWalking");
-
-    private void Start()
+    [RequireComponent(typeof(Animator))]
+    public class Animation : MonoBehaviour
     {
-        _animator = GetComponent<Animator>();
+        private Animator _animator;
+        private static readonly int MovementX = Animator.StringToHash("MovementX");
+        private static readonly int MovementY = Animator.StringToHash("MovementY");
+        private static readonly int isWalking = Animator.StringToHash("isWalking");
+    
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+    
+        public void Walk(Vector2 direction)
+        {
+            if (direction != Vector2.zero)
+            {
+                _animator.SetFloat(MovementX, direction.x);
+                _animator.SetFloat(MovementY, direction.y);
+                _animator.SetBool(isWalking, true);
+            }
+            else
+            {
+                _animator.SetBool(isWalking, false);
+            }
+        }
     }
 
-    public void Walk(Vector2 direction)
-    {
-        if (direction != Vector2.zero)
-        {
-            _animator.SetFloat(MovementX, direction.x);
-            _animator.SetFloat(MovementY, direction.y);
-            _animator.SetBool(isWalking, true);
-        }
-        else
-        {
-            _animator.SetBool(isWalking, false);
-        }
-    }
 }
