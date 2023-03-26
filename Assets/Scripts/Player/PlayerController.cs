1be0b5e22ec16e448aba9e31f3d5343a6818178f
double-hope <nadia.prohorchuk@gmail.com>
2023-03-27 00:04:09 +0300

    simplify code

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,23 +1,26 @@
+using Core.Movement.Controller;
+using Core.Movement.Data;
 using UnityEngine;
+using Animation = Core.Animation.Animation;
 
 namespace Player
 {
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float _movementSpeed;
+        [SerializeField] private Animation _animation;
+        [SerializeField] private DirectionalMovementData _directionalMovementData;
         private Rigidbody2D _rigidbody;
+        private DirectionalMover _directionalMover;
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _directionalMover = new DirectionalMover(_rigidbody, _directionalMovementData);
         }
 
-        public void MoveXY(Vector2 direction)
-        {
-            Vector2 velocity = _rigidbody.velocity;
-            velocity = direction * _movementSpeed;
-            _rigidbody.velocity = velocity;
-        }
-        
+
+        public void MoveXY(Vector2 direction) => _directionalMover.MoveXY(direction);
+
+        public void UpdateAnimation(Vector2 direction) => _animation.Walk(direction);
     }
 }
