605ed99e87c1d4e608960bfb823fa169fece2db0
double-hope <nadia.prohorchuk@gmail.com>
2023-03-27 01:10:12 +0300

    add stat system

---
+++ Assets/Scripts/StatsSystem/Enum/StatType.cs
@@ -0,0 +1,10 @@
+﻿namespace StatsSystem.Enum
+{
+    public enum StatType
+    {
+        None = 0,
+        Health = 1,
+        Damage = 2,
+        Speed = 3,
+    }
+}
---
+++ Assets/Scripts/StatsSystem/IStatValueGiven.cs
@@ -0,0 +1,9 @@
+﻿using StatsSystem.Enum;
+
+namespace StatsSystem
+{
+    public interface IStatValueGiven
+    {
+        float GetStatValue(StatType statType);
+    }
+}
