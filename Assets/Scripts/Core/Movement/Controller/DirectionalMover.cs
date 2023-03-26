7cd410d1fa4c7317632136d63ec6ff71a9a5fef7
double-hope <nadia.prohorchuk@gmail.com>
2023-03-27 00:06:10 +0300

    create controller for moving

---
+++ Assets/Scripts/Core/Movement/Controller/DirectionalMover.cs
@@ -0,0 +1,24 @@
+﻿using Core.Movement.Data;
+using UnityEngine;
+
+namespace Core.Movement.Controller
+{
+    public class DirectionalMover
+    {
+        private readonly Rigidbody2D _rigidbody;
+        private readonly DirectionalMovementData _directionalMovementData;
+
+        public DirectionalMover(Rigidbody2D rigidbody, DirectionalMovementData directionalMovementData)
+        {
+            _rigidbody = rigidbody;
+            _directionalMovementData = directionalMovementData;
+        }
+        
+        public void MoveXY(Vector2 direction)
+        {
+            Vector2 velocity = _rigidbody.velocity;
+            velocity = direction * _directionalMovementData.MovementSpeed;
+            _rigidbody.velocity = velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Movement/Data/DirectionalMovementData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Movement.Data
+{
+    [Serializable]
+    public class DirectionalMovementData
+    {
+        [field: SerializeField] public float MovementSpeed { get; private set; }
+    }
+}
