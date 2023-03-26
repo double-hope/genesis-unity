d11e854eba2e9e4dcf0264a769cc857e142de818
double-hope <nadia.prohorchuk@gmail.com>
2023-03-27 00:04:37 +0300

    create PlayerSystem to control player

---
+++ Assets/Scripts/Player/PlayerSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using InputReader;
+
+namespace Player
+{
+    public class PlayerSystem
+    {
+        private readonly PlayerController _playerController;
+        private readonly PlayerBrain _playerBrain;
+
+        public PlayerSystem(PlayerController playerController, List<IEntityInputSource> inputSources)
+        {
+            _playerController = playerController;
+            _playerBrain = new PlayerBrain(_playerController, inputSources);
+        }
+    }
+}
